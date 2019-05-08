2e0e71979e14c84f6c4cd4e77e756ad70bcf3dcc
Robin <marzg.zl55@gmail.com>
2019-05-07 21:28:16 -0500

    transiciones

--- evaluador/evaluador.py
+++ evaluador/evaluador.py
@@ -17,8 +17,8 @@ class Evaluador:
         self.automata.conectar("c","#","#","p","q")
         self.automata.conectar("c","b","b","p","q")
         self.automata.conectar("c","a","a","p","q")
-        self.automata.conectar("b","b","λ","q","q")
-        self.automata.conectar("a","a","λ","q","q")
+        self.automata.conectar("b","b","*","q","q")
+        self.automata.conectar("a","a","*","q","q")
         self.automata.conectar("*","#","#","q","r")
 
     def evaluar(self, palabra):
